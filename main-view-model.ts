5d33674ee308238213a8c308765da1e4874a7f1c
Nathan Walker <walkerrunpdx@gmail.com>
2016-08-26 09:26:12 -0700

    consistency

--- main-view-model.ts
+++ main-view-model.ts
@@ -20,7 +20,7 @@ export class HelloWorldModel extends Observable {
   set message(value: string) {
     if (this._message !== value) {
       this._message = value;
-      this.notifyPropertyChange("message", value)
+      this.notifyPropertyChange('message', value)
     }
   }
 
@@ -31,9 +31,9 @@ export class HelloWorldModel extends Observable {
 
   private updateMessage() {
     if (this._counter <= 0) {
-      this.message = "Hoorraaay! You unlocked the NativeScript clicker achievement!";
+      this.message = 'Hoorraaay! You unlocked the NativeScript clicker achievement!';
     } else {
-      this.message = this._counter + " taps left";
+      this.message = `${this._counter} taps left`;
     }
   }
 }
